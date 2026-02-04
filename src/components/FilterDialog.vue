1e9cb363b57bfe2a4b932f299570feb15736fdfe
the3ver <the3ver@pm.me>
2026-02-04 21:45:34 +0100

    feat: added type filter for calendar module

--- src/components/FilterDialog.vue
+++ src/components/FilterDialog.vue
@@ -14,10 +14,10 @@ const emit = defineEmits(['update:modelValue', 'update:selectedFilters'])
 const { t } = useI18n()
 
 const availableTypes = computed(() => [
-  { title: t('calendar.types.doctor'), value: 'doctor' },
-  { title: t('calendar.types.vaccination'), value: 'vaccination' },
-  { title: t('calendar.types.illness'), value: 'illness' },
-  { title: t('calendar.types.note'), value: 'note' }
+  { title: t('calendar.types.doctor'), value: 'doctor', icon: 'mdi-doctor', color: 'primary' },
+  { title: t('calendar.types.vaccination'), value: 'vaccination', icon: 'mdi-needle', color: 'teal' },
+  { title: t('calendar.types.illness'), value: 'illness', icon: 'mdi-emoticon-sick', color: 'orange' },
+  { title: t('calendar.types.note'), value: 'note', icon: 'mdi-note-text', color: 'blue-grey' }
 ])
 
 const localFilters = computed({
@@ -35,15 +35,19 @@ const close = () => {
     <v-card>
       <v-card-title>{{ t('calendar.filter') }}</v-card-title>
       <v-card-text>
-        <v-checkbox
-          v-for="type in availableTypes"
-          :key="type.value"
-          v-model="localFilters"
-          :label="type.title"
-          :value="type.value"
-          hide-details
-          density="compact"
-        ></v-checkbox>
+        <div v-for="type in availableTypes" :key="type.value" class="d-flex align-center mb-2">
+          <v-avatar :color="type.color" size="32" class="mr-3">
+            <v-icon color="white" size="18">{{ type.icon }}</v-icon>
+          </v-avatar>
+          <v-checkbox
+            v-model="localFilters"
+            :label="type.title"
+            :value="type.value"
+            hide-details
+            density="compact"
+            class="flex-grow-1"
+          ></v-checkbox>
+        </div>
       </v-card-text>
       <v-card-actions>
         <v-spacer></v-spacer>
